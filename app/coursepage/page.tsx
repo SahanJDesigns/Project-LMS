f12fd48e9ce0bda0c22d0af665b70d7ba21c25a9
NaveenSandaruwan <157776142+NaveenSandaruwan@users.noreply.github.com>
2024-12-27 19:29:05 +0530

    Enhance CoursePage layout and update PageSearchBox for improved functionality and design

--- app/coursepage/page.tsx
+++ app/coursepage/page.tsx
@@ -92,7 +92,7 @@ const CoursePage: React.FC = () => {
               </button>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
             {courses.map((course, index) => (
               <CourseCard
                 key={index}
