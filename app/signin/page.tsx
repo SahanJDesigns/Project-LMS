6b1964828ed1c4121635bd814e8d1a5096f3365e
Janodi Jayathunga <79194620+20061205@users.noreply.github.com>
2024-12-30 17:42:00 +0530

    Refactor login page layout and styles for improved responsiveness and visual appeal

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,26 +2,29 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="flex min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500">
+  <div className="font-sans min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="flex min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 my-20">
       {/* Left Section */}
-      <div className="w-1/2 flex items-center justify-center p-10">
+      <div className="w-1/3 flex items-center justify-center p-10">
         <div className="text-white">
           <h1 className="text-4xl font-bold mb-4">
             Very good works are waiting for you
           </h1>
           <p className="text-2xl font-semibold">Login Now!!!</p>
-          <div className="mt-8">
+            <div className="mt-10">
             <img
               src="/contact-10.svg"
               alt="Illustration"
-              className="w-full max-w-md"
+              width={1000}
+              height={1000}
+              className="w-full max-w-lg"
             />
-          </div>
+            </div>
         </div>
       </div>
 
       {/* Right Section */}
-      <div className="w-1/2 bg-white flex flex-col items-center justify-center p-10">
+      <div className="w-2/3 bg-white flex flex-col items-center justify-center p-10">
         <h2 className="text-2xl font-bold mb-2">SIGN IN</h2>
         <p className="text-gray-500 mb-6">
           How do I get started lorem ipsum dolor at?
@@ -81,6 +84,7 @@ const LoginPage = () => {
         </p>
       </div>
     </div>
+  </div>
   );
 };
 
