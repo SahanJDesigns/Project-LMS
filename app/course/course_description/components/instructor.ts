8d800f58f72cfbb685f83053538d7e89cd4cc280
Dineth-2001 <157557333+Dineth-2001@users.noreply.github.com>
2025-01-03 00:47:01 +0530

    course page - frontend

--- app/course/course_description/components/instructor.ts
+++ app/course/course_description/components/instructor.ts
@@ -1,13 +1,21 @@
 export interface InstructorDetails {
     name: string;
+    role: string;
     bio: string;
     image: string;
+    totalReviews: number;
+    totalStudents: number;
+    totalCourses: number;
 }
   
 export const instructor: InstructorDetails = {
     name: "Ronald Richards",
+    role: "UI/UX Designer",
     bio: "With over a decade of industry experience, Ronald brings a wealth of practical knowledge to the classroom. He has played a pivotal role in designing user-centric interfaces for renowned tech companies, ensuring seamless and engaging user experiences.",
     image: "/ronald.jpg",
+    totalReviews: 11023,
+    totalStudents: 500,
+    totalCourses: 15,
 };
   
 
